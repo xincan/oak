2e978cfa0c05f079940914dd488ec8061e578669
jiangxincan <alittlexincan@163.com>
2022-09-27 16:32:53 +0800

    优化代码：当天工时与当天总项目树挂钩

---
+++ src/main/java/cn/com/tarotframework/server/oak/service/ISysUserPostService.java
@@ -0,0 +1,12 @@
+package cn.com.tarotframework.server.oak.service;
+
+import cn.com.tarotframework.server.oak.po.SysProject;
+
+import java.util.List;
+
+
+public interface ISysUserPostService  {
+
+    void insert(List<SysProject> projects);
+
+}
