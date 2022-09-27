479f45ce14bad73aab37c0c017eff6477fe20dac
jiangxincan <alittlexincan@163.com>
2022-09-27 13:55:44 +0800

    增加oak项目

--- src/main/java/cn/com/tarotframework/OakApplication.java
+++ src/main/java/cn/com/tarotframework/OakApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TarotDemoApplication {
+public class OakApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TarotDemoApplication.class, args);
+        SpringApplication.run(OakApplication.class, args);
     }
 
 }
--- src/test/java/cn/com/tarotframework/OakApplicationTests.java
+++ src/test/java/cn/com/tarotframework/OakApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TarotDemoApplicationTests {
+class OakApplicationTests {
 
     @Test
     void contextLoads() {
